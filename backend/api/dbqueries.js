72e0f96f5a648cdf27289b0af1982acd4b6a549a
wguzik <wojciech.guzik@dhl.com>
2022-09-08 21:13:41 +0200

    Backend init

---
+++ backend/api/dbqueries.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: 5432,
+})
+
+const getArticles = (request, response) => {
+    pool.query('SELECT * FROM articles ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getArticles
+  }
