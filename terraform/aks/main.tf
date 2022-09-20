42735ae149abdd09eac11deb027f5a6feaa29cca
wguzik <wojciech.guzik@dhl.com>
2022-09-20 15:57:52 +0200

    missing reference

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -67,6 +67,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "appworkload" {
 resource "azurerm_role_assignment" "akstoacrrole" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = data.azurerm_container_registry.acr.id
+  scope                            = data.azurerm_container_registry.cr.id
   skip_service_principal_aad_check = true
 }
