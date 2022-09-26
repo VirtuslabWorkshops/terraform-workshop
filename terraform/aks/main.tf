5f758a2742469b7406c9260255244fb3bf96a210
wguzik <wojciech.guzik@dhl.com>
2022-09-26 22:49:12 +0200

    change vm sizign for aks

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "appworkload" {
   node_count            = 1
   enable_auto_scaling   = false
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS3_v2"
+  vm_size               = "Standard_D1_v2"
   vnet_subnet_id        = var.vnet_subnet_id_app_workload
 
   tags = local.tags
