a2430265e32f39d919aa7324f0f3fb40c97a2f2b
Michał Ogrodnik <michal.ogrodnik@tesco.com>
2023-03-27 19:39:21 +0200

    Remove unused parts

--- 09-lab-ci/infra/rg/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "rg-name" {
-  description = "RG name"
-  value       = azurerm_resource_group.rg.name
-}
--- lab07/infra/template/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  postfix         = "${var.workload}-${var.environment}-${var.location}"
-  postfix_no_dash = replace(local.postfix, "-", "")
-  rg_group_name   = "rg-${local.postfix}"
-}
-
-data "azurerm_client_config" "current" {}
